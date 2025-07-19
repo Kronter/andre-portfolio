62ba716f777c223f63266480c7848fd867196917
Andre <andre.gottgtroy@mytona.com>
2025-07-20 00:16:00 +1200

    Animation fixes

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -196,7 +196,7 @@ const handleCopyEmail = () => {
                                 {copySuccess}
                             </motion.div>
                         )}
-                    </AnimatePresence>
+                        </AnimatePresence>
                     <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-8">
                         <a href={asset("/Andre_Gottgtroy_Resume.pdf")} download className="inline-flex items-center px-8 py-3 border-2 border-violet-500 text-violet-400 font-bold rounded-lg hover:bg-violet-500 hover:text-white transition-all duration-300 text-lg">
                             <FileText className="w-5 h-5 mr-2" />
